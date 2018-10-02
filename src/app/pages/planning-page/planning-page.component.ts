d10d501edd055194cd7965f440055ea575a4802b
Sven Röttering <sven.roettering@10m.de>
2018-10-02 16:12:05 +0200

    Add editing for most stuff

--- src/app/pages/planning-page/planning-page.component.ts
+++ src/app/pages/planning-page/planning-page.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
-import { Moment, utc } from 'moment';
+import { Moment, parseZone } from 'moment';
 import { Observable } from 'rxjs/internal/Observable';
 import { Barcamp } from '../../models/barcamp';
 import { SlotType } from '../../models/slot-type';
 import { Time } from '../../models/time';
-import { AddDay, AddRoom, AddTimeSlot } from '../../state/planning/planning.actions';
+import { AddDay, AddRoom, AddTimeSlot, EditRoom, EditTimeSlot } from '../../state/planning/planning.actions';
 import { PlanningState } from '../../state/planning/planning.state';
 import { StompSubscribe } from '../../state/stomp/stomp.actions';
 
@@ -21,13 +21,13 @@ export class PlanningPageComponent {
     barcamp: Observable<Barcamp>;
 
     @Select(PlanningState.days)
-    days: Observable<Moment>;
+    days: Observable<Moment[]>;
 
     @Select(PlanningState.times)
-    times: Observable<Time>;
+    times: Observable<Time[]>;
 
     @Select(PlanningState.rooms)
-    rooms: Observable<string>;
+    rooms: Observable<string[]>;
 
     constructor(private store: Store) {
         this.store.dispatch([
@@ -41,20 +41,46 @@ export class PlanningPageComponent {
     }
 
     addDay() {
-        const lastDay = this.store.selectSnapshot(PlanningState.days).reverse()[0];
-        const newDay = lastDay ? lastDay.clone().add(1, 'day') : utc();
+        const days = this.store.selectSnapshot(PlanningState.days);
+        const lastDay = days[days.length - 1];
+        const newDay = lastDay ? lastDay.clone().add(1, 'day') : parseZone();
         this.store.dispatch(new AddDay(newDay));
     }
 
     addRoom() {
-        this.store.dispatch(new AddRoom(`Room ${(Math.random() * 10 % 10).toFixed(0)}`));
+        this.store.dispatch(
+            new AddRoom(`Room ${this.store.selectSnapshot(state => state.planning.rooms).length + 1}`)
+        );
     }
 
     addTime() {
+        const times: Time[] = this.store.selectSnapshot(state => state.planning.times);
+        const latestTime = times[times.length - 1];
+
         this.store.dispatch(new AddTimeSlot({
-            start: utc().startOf('hour').hours(9),
-            end: utc().startOf('hour').hours(10),
+            start: latestTime
+                ? latestTime.end.clone().add(15, 'minute')
+                : parseZone().local().startOf('hour').hours(9),
+            end: latestTime
+                ? latestTime.end.clone().add(1, 'hour')
+                : parseZone().local().startOf('hour').hours(10),
             type: SlotType.TOPIC
         }));
+    }
+
+    roomNameChange(event: any, roomName: string, roomIndex: number) {
+        const newName = event.target.innerText;
+        if (newName && newName !== roomName) {
+            this.store.dispatch(new EditRoom({ roomIndex, newName }));
+        }
+    }
+
+    startTimeChange(event: any, { start, ...rest }: Time, timeSlotIndex: number) {
+        console.log(start);
+        const time = parseZone(event.target.innerText, 'H:mm').local();
+        console.log(time);
+        if (time && !time.isSame(start)) {
+            this.store.dispatch(new EditTimeSlot({ timeSlotIndex, newTime: { start: time, ...rest } }));
+        }
     }
 }
